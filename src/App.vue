a093d637d9d894e4d2068739c3dc678b66cb10b9
akerimbaeva <akerimbaeva@gmail.com>
2021-04-15 17:55:15 +0600

    commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template> 
+  <div class="container">
+      {{name}}
+  </div>
+  </template>
+<script>
+
+export default {
+  data(){
+    return {
+      name: "Anara"
+    }
+  }
+}
+</script>
+<style>
+.container {
+  color: red;
+  margin: 0 auto;
+  width: 80%;
+  font-size: 28px;
+
+}
+</style>
